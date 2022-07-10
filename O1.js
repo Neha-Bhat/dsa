559bd26944f3fdf1472fd7263eb27d219b7e4018
Neha-Bhat <45681122+Neha-Bhat@users.noreply.github.com>
2022-07-10 19:11:45 +0530

    O(1) time complexity

---
+++ O1.js
@@ -0,0 +1,5 @@
+// If the operation remains constant irrespective of the element size, then it is said to have constant time complexity
+
+let arr = new Array(1000);
+arr.fill("test");
+console.log(arr[0]);
